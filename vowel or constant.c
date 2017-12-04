290e11ed0ec33949244e133ba00023c052009307
DHIVYABHARATIM <31499436+DHIVYABHARATIM@users.noreply.github.com>
2017-12-04 12:04:23 +0530

    Create vowel or constant.c

---
+++ vowel or constant.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int c;
+scanf("%c",&c);
+if(c = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'));
+printf("c is a vowel");
+else
+printf("c is an alphabet");
+}
