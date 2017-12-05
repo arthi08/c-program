824145f954bcd1d35ead60bfc1de4f6ea228692f
DHIVYABHARATIM <31499436+DHIVYABHARATIM@users.noreply.github.com>
2017-12-05 12:51:58 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int real;
+int exponent;
+scanf("%d%d",&real,&exponent);
+ while (exponent != 0)
+    {
+        result *= base;
+        --exponent;
+    }
+printf("%d",result);
+}
