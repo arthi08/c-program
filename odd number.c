2f99d00a78ade2de182499dfdca5cc0c9cdf4880
DHIVYABHARATIM <31499436+DHIVYABHARATIM@users.noreply.github.com>
2017-12-05 13:21:32 +0530

    Create odd number.c

---
+++ odd number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>  
+   
+int main()
+{  
+    int counter;
+    for(counter = 1; counter <= 100; counter++)
+    { 
+        if(counter%2 == 1)
+            printf("%d ", counter);  
+        }  
+    
+   
+} 
