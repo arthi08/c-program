2f2bae9fb97aad242298cd12fed64275b6075587
DHIVYABHARATIM <31499436+DHIVYABHARATIM@users.noreply.github.com>
2018-01-20 17:27:45 +0530

    Update armstorng number153.c

--- armstorng number153.c
+++ armstorng number153.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
-
 void main()
 {
-	int num,rem,result=0;
-	scanf("%d",&num);
-	while(num!=0)
-	{
-		rem=num%10;
-		result=result+rem*rem*rem;
-		num=num/10;
-	
-	}
-	if(result==num)
-	printf("yes,armstrong");
-	else
-	printf("not armstrong num");
-	// your code goes here
-	return 0
-}
+  int n1, n2, i, temp, num, rem;
+  printf("Enter interval");
+  scanf("%d %d", &n1, &n2);
+  printf("Armstrong numbers between %d an %d are", n1, n2);
+  for(i=n1+1; i<n2; ++i)
+  {
+	  temp=i;
+      num=0;
+      while(temp!=0)
+      {
+          rem=(temp%10);
+          num+=rem*rem*rem;
+          temp/=10;
+      }
+      if(i==num)
+          printf("%d ",i);
+      }
+  }
